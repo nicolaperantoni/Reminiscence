3fee51ff772eb7437f1691f1477d121e580b996d
jibbo <posta.giovanni.defrancesco@gmail.com>
2013-03-07 22:41:05 +0100

    Project started

---
+++ src/it/unitn/vanguard/reminiscence/SelectActivity.java
@@ -0,0 +1,23 @@
+package it.unitn.vanguard.reminiscence;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class SelectActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.select, menu);
+        return true;
+    }
+    
+}
