365c27b85b9479a7c092168d28af57f873b03864
Giulio Fornasaro <giulio.fornasaro@gmail.com>
2013-03-11 22:57:57 +0100

    Continuando ancora a lavorare sulla PasswordActivity

--- src/it/unitn/vanguard/reminiscence/PasswordActivity.java
+++ src/it/unitn/vanguard/reminiscence/PasswordActivity.java
@@ -1,7 +1,11 @@
 package it.unitn.vanguard.reminiscence;
 
+import java.util.Date;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.ContactsContract.CommonDataKinds.Email;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -16,14 +20,31 @@ public class PasswordActivity extends Activity {
 	private EditText mPasswordEditText;
 	private Button mBackButton;
 	private Button mConfirmButton;
+	
+	private String name;
+	private String surname;
+	private String eMail;
+	private String day;
+	private String month;
+	private String year;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_registration_password);
 
+		dispatchInfoFromIntent(getIntent());
 		initializeButton();
 		initializeListeners();
+	}
+	
+	private void dispatchInfoFromIntent(Intent i) {
+		name=i.getExtras().getString("name");
+		surname=i.getExtras().getString("surname");
+		eMail=i.getExtras().getString("email");
+		day=i.getExtras().getString("day");
+		month=i.getExtras().getString("month");
+		year=i.getExtras().getString("year");
 	}
 
 	private void initializeButton() {
