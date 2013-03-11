3645d7eb792b94f2362f6fa5a1c312fe0dd78c27
Giulio Fornasaro <giulio.fornasaro@gmail.com>
2013-03-11 22:10:50 +0100

    Cominciato a lavorare sul PasswordActivity

---
+++ src/it/unitn/vanguard/reminiscence/PasswordActivity.java
@@ -0,0 +1,15 @@
+package it.unitn.vanguard.reminiscence;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PasswordActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_registration_password);
+	}
+	
+	
+}
