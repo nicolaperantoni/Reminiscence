8b364df3485c7972b26c70652052123f7215379f
Giulio Fornasaro <giulio.fornasaro@gmail.com>
2013-03-18 17:49:09 +0100

    fixati i link tra le activity di registrazione

--- src/it/unitn/vanguard/reminiscence/PasswordActivity.java
+++ src/it/unitn/vanguard/reminiscence/PasswordActivity.java
@@ -38,7 +38,7 @@ public class PasswordActivity extends Activity implements OnTaskFinished {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_registration_password);
-		//dispatchInfoFromIntent(getIntent());
+		dispatchInfoFromIntent(getIntent());
 		initializeButtons();
 		generateSuggestion();
 		initializeListeners();
