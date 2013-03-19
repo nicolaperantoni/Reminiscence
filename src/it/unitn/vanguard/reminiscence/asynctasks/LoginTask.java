05fef2107ba7eb64e85d916a8f32beaa414c1dea
Zambo <lucazamboni92@gmail.com>
2013-03-19 09:22:50 +0100

    Messo a posto il login task

--- src/it/unitn/vanguard/reminiscence/asynctasks/LoginTask.java
+++ src/it/unitn/vanguard/reminiscence/asynctasks/LoginTask.java
@@ -1,17 +1,21 @@
 package it.unitn.vanguard.reminiscence.asynctasks;
 
+import it.unitn.vanguard.reminiscence.interfaces.OnTaskFinished;
+import it.unitn.vanguard.reminiscence.utils.Constants;
+
+import java.io.UnsupportedEncodingException;
 import java.util.ArrayList;
 
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import it.unitn.vanguard.reminiscence.interfaces.OnTaskFinished;
-import it.unitn.vanguard.reminiscence.utils.Constants;
 import android.os.AsyncTask;
 import android.util.Log;
 
@@ -39,27 +43,23 @@ public class LoginTask extends AsyncTask<String, Void, Boolean> {
 		
 		HttpClient client = new DefaultHttpClient();
 		HttpPost post = new HttpPost(Constants.SERVER_URL + "login.php");
-		post.setEntity(null);
-		json = null;
-		String content = null;
-		
 		try {
-			content = client.execute(post).getEntity().getContent().toString();
-			json = new JSONObject(content);
-			Log.i(LoginTask.class.getName(), content);
+			post.setEntity(new UrlEncodedFormEntity(params));
+		} catch (UnsupportedEncodingException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		json = null;
+		String jsonString;
+		try {
+			jsonString = EntityUtils.toString(client.execute(post).getEntity());
+			json = new JSONObject(jsonString);
 		} catch (Exception e) {
 			this.ex = e;
 			return false;
 		}
 		
-		String retval;
-		try {
-			retval = json.getString("success");
-		} catch (JSONException e) {
-			retval = null;
-		}
-		
-		return (retval=="true") ? true : false;
+		return true;
 	}
 	
 	@Override
