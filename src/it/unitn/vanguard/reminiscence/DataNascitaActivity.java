88162e9d1eba20f4e93a76e26f22bcfbc0798b10
robzenn92 <rob.zenn92@hotmail.it>
2013-03-19 11:51:41 +0100

    modifiche in DataNascita ancora da sistemare

--- src/it/unitn/vanguard/reminiscence/DataNascitaActivity.java
+++ src/it/unitn/vanguard/reminiscence/DataNascitaActivity.java
@@ -11,31 +11,98 @@ import android.widget.TextView;
 
 public class DataNascitaActivity extends Activity {
 
+	// Date values
+	private int day = 1, month = 1, year = 1950;
+	
+	// Day views
+	private Button btnDayUp, btnDayDown;
+	private TextView txtDay;
+	
+	// Month Views
 	private Button btnMonthUp, btnMonthDown;
-	private Button mBackButton;
-	private Button mConfirmButton;
 	private TextView txtMonth;
+	
+	// Year Views
+	private Button btnYearUp, btnYearDown;
+    private TextView  txtYear;
+	
+    // Back - Confirm Buttons
+	private Button btnBack;
+	private Button btnConfirm;
+	private String[] mesi = {"Gennaio","Febbraio","Marzo","Aprile","Maggio","Giugno",
+	        "Luglio","Agosto","Settembre","Ottobre","Novembre","Dicembre"};
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_data_nascita);
-		
 		initializeButtons();
 		initializeListeners();
-		txtMonth.setText("Novembre");
 	}
 	
 	private void initializeButtons() {
+		
+		//add day buttons
+        btnDayUp = (Button) findViewById(R.id.btn_day_up);
+        btnDayDown = (Button) findViewById(R.id.btn_day_down);
+        txtDay = (TextView) findViewById(R.id.txt_day);
+		
+		//add month buttons
 		btnMonthUp = (Button) findViewById(R.id.btn_month_up);
 		btnMonthDown = (Button) findViewById(R.id.btn_month_down);
 		txtMonth = (TextView) findViewById(R.id.txt_month);
-		mBackButton = (Button) findViewById(R.id.registration_back_btn);
-		mConfirmButton = (Button) findViewById(R.id.registration_confirm_btn);
+		
+        //add year buttons
+        btnYearUp = (Button) findViewById(R.id.btn_year_up);
+        btnYearDown = (Button) findViewById(R.id.btn_year_down);
+        txtYear = (TextView) findViewById(R.id.txt_year);
+        
+		btnBack = (Button) findViewById(R.id.registration_back_btn);
+		btnConfirm = (Button) findViewById(R.id.registration_confirm_btn);
 	}
 
 	private void initializeListeners() {
-		mBackButton.setOnClickListener(new OnClickListener() {
+		
+		// DAY UP-DOWN EVENTS
+		btnDayUp.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				txtDay.setText("" + (++day) % 32);
+			}
+		});
+		
+		btnDayDown.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				txtDay.setText("" + (--day) % 32);
+			}
+		});
+		
+		// MONTH UP-DOWN EVENTS
+		btnMonthUp.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				txtMonth.setText(mesi[(++month) % ( mesi.length + 1)]);
+			}
+		});
+		
+		btnMonthDown.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				txtMonth.setText(mesi[ (--month) % (mesi.length + 1)]);
+			}
+		});
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		btnBack.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
@@ -44,7 +111,7 @@ public class DataNascitaActivity extends Activity {
 			}
 		});
 		
-		mConfirmButton.setOnClickListener(new OnClickListener() {
+		btnConfirm.setOnClickListener(new OnClickListener() {
 			
 			@Override
 			public void onClick(View v) {
