4c344dc90fbf7842788417d2b211f25e6b5e17cb
robzenn92 <rob.zenn92@hotmail.it>
2013-03-19 09:21:15 +0100

    add intent parameters in RegistrationActivity

--- src/it/unitn/vanguard/reminiscence/RegistrationActivity.java
+++ src/it/unitn/vanguard/reminiscence/RegistrationActivity.java
@@ -6,11 +6,15 @@ import android.content.Intent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class RegistrationActivity extends Activity {
 
 	private Button btnBack;
 	private Button btnConfirm;
+	private EditText editTextName;
+	private EditText editTextSurname;
+	private EditText editTextMail;
 	
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,14 +34,24 @@ public class RegistrationActivity extends Activity {
     private void initializeButtons() {
     	btnBack = (Button) findViewById(R.id.registration_back_btn);
     	btnConfirm = (Button) findViewById(R.id.registration_confirm_btn);
+    	editTextName = (EditText) findViewById(R.id.editTextregistrationName);
+    	editTextSurname = (EditText) findViewById(R.id.editTextregistrationSurname);
+    	editTextMail = (EditText) findViewById(R.id.editTextregistrationMail);
 	}
 	
 	private void initializeListeners() {
 		btnConfirm.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
-				// TODO call previous activity
+				
+				String name = editTextName.getText().toString();
+				String surname = editTextSurname.getText().toString();
+				String mail = editTextMail.getText().toString();
+				
 				Intent registrationIntent = new Intent(v.getContext(), DataNascitaActivity.class);
+				registrationIntent.putExtra("name",name);
+				registrationIntent.putExtra("surname",surname);
+				registrationIntent.putExtra("email",mail);
 		        startActivityForResult(registrationIntent, 0);
 			}
 		});
