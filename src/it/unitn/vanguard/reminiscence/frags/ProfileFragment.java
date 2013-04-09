bed72b46ea85a58a0e8a86396640b04bd95303d7
Giulio Fornasaro <giulio.fornasaro@gmail.com>
2013-04-09 16:17:31 +0200

    comincio a lavorare sul fragment per il profilo

---
+++ src/it/unitn/vanguard/reminiscence/frags/ProfileFragment.java
@@ -0,0 +1,11 @@
+package it.unitn.vanguard.reminiscence.frags;
+
+import android.support.v4.app.Fragment;
+import android.widget.Button;
+import android.widget.ImageView;
+
+public class ProfileFragment extends Fragment {
+	
+	private ImageView profilePhoto;
+
+}
