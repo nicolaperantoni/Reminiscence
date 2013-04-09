64aeaa6ef5f15d6c67c4eaf5cffdcd636ebc945f
Giulio Fornasaro <giulio.fornasaro@gmail.com>
2013-04-09 17:32:13 +0200

    lavorando sull'xml.. i know... i'm sloooow

--- src/it/unitn/vanguard/reminiscence/frags/ProfileFragment.java
+++ src/it/unitn/vanguard/reminiscence/frags/ProfileFragment.java
@@ -1,11 +1,32 @@
 package it.unitn.vanguard.reminiscence.frags;
 
+import it.unitn.vanguard.reminiscence.R;
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class ProfileFragment extends Fragment {
-	
+
 	private ImageView profilePhoto;
+	private String name;
+	private String surname;
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.fragment_story, container, false);
+		return v;
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+		
+		
+	}
 
 }
