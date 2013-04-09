cf3f391ee5d1e2aee5e372d63550fd98e00e3d1e
Giulio Fornasaro <giulio.fornasaro@gmail.com>
2013-04-09 17:34:26 +0200

    dimenticato di aggiungere i nuovi file

--- src/it/unitn/vanguard/reminiscence/frags/ProfileFragment.java
+++ src/it/unitn/vanguard/reminiscence/frags/ProfileFragment.java
@@ -18,7 +18,7 @@ public class ProfileFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-		View v = inflater.inflate(R.layout.fragment_story, container, false);
+		View v = inflater.inflate(R.layout.fragment_profile, container, false);
 		return v;
 	}
 
