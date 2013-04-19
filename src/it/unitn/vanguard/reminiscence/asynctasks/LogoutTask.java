ba7ed4b9fd5d921977dfa182397b34b5ea0fcc51
robzenn92 <rob.zenn92@hotmail.it>
2013-04-20 01:56:24 +0200

    add logout task

---
+++ src/it/unitn/vanguard/reminiscence/asynctasks/LogoutTask.java
@@ -0,0 +1,85 @@
+package it.unitn.vanguard.reminiscence.asynctasks;
+
+import it.unitn.vanguard.reminiscence.interfaces.OnTaskFinished;
+import it.unitn.vanguard.reminiscence.utils.Constants;
+
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
+import android.util.Log;
+
+public class LogoutTask extends AsyncTask<String, Void, Boolean> {
+
+	private OnTaskFinished caller;
+	private Exception ex;
+	private JSONObject json;
+
+	public LogoutTask(OnTaskFinished caller) {
+		super();
+		this.caller = caller;
+	}
+
+	@Override
+	protected Boolean doInBackground(String... arg0) {
+
+		if (arg0.length < 2) {
+			throw new IllegalStateException("You should pass at least 2 params");
+		}
+
+		ArrayList<NameValuePair> params = new ArrayList<NameValuePair>(2);
+		params.add(new BasicNameValuePair("email", arg0[0]));
+		params.add(new BasicNameValuePair("password", arg0[1]));
+
+		HttpClient client = new DefaultHttpClient();
+		HttpPost post = new HttpPost(Constants.SERVER_URL + "logout.php");
+		try {
+			post.setEntity(new UrlEncodedFormEntity(params));
+		} catch (UnsupportedEncodingException e1) {
+			e1.printStackTrace();
+		}
+		json = null;
+		String jsonString;
+		try {
+			jsonString = EntityUtils.toString(client.execute(post).getEntity());
+			json = new JSONObject(jsonString);
+			if (json != null && json.getString("success").equals("true")) {
+				SharedPreferences prefs = PreferenceManager
+						.getDefaultSharedPreferences(((Activity) caller)
+								.getApplicationContext());
+				SharedPreferences.Editor editor = prefs.edit();
+				editor.putString("email", arg0[0]);
+				editor.putString("password", arg0[1]);
+				editor.putString("token", json.getString("token"));
+				editor.commit();
+			}
+
+		} catch (Exception e) {
+			this.ex = e;
+			return false;
+		}
+
+		return true;
+	}
+
+	@Override
+	protected void onPostExecute(Boolean result) {
+		super.onPostExecute(result);
+		if (!result && ex != null) {
+			Log.e(RegistrationTask.class.getName(), ex.toString());
+		}
+		caller.onTaskFinished(json);
+	}
+}
