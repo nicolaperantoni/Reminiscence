6b8d5564daca6483c9152163d436472ea9e54e98
Mirko <mirko.zaff@gmail.com>
2013-04-22 22:45:32 +0200

    prova di friendlist

--- src/it/unitn/vanguard/reminiscence/ViewFriendsProfileFragmentActivity.java
+++ src/it/unitn/vanguard/reminiscence/ViewFriendsProfileFragmentActivity.java
@@ -6,22 +6,34 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
 
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 public class ViewFriendsProfileFragmentActivity extends FragmentActivity {
 	
 	private Context context;
+	
+	String[] names = new String[]{"Marco", "Lucia", "Francesco", "Luca", "Erica", "Francesca", "Giovanni"
+			, "Nicola", "Mario", "Ignazio"};
+	
+	String[] surnames = new String[]{"Rossi", "Bacchi", "Padovan", "Cabella", "Iacchini", "Zen"
+			, "Fabbri", "Radini", "Verdi", "Zago"};
 
+	int[] flags = new int[]{R.drawable.default_profile_image,
+							R.drawable.default_profile_image,
+							R.drawable.default_profile_image,
+							R.drawable.default_profile_image,
+							R.drawable.default_profile_image,
+							R.drawable.default_profile_image,
+							R.drawable.default_profile_image,
+							R.drawable.default_profile_image,
+							R.drawable.default_profile_image,
+							R.drawable.default_profile_image,};
+	
 	@Override
 	protected void onCreate(Bundle arg0) {
 		super.onCreate(arg0);
@@ -29,29 +41,24 @@ public class ViewFriendsProfileFragmentActivity extends FragmentActivity {
 		context = getApplicationContext();
 		String language = FinalFunctionsUtilities.getSharedPreferences("language", context);
 		FinalFunctionsUtilities.switchLanguage(new Locale(language), context);
-		setContentView(R.layout.activity_viewfriendprofile);
-		
-		String[] names = new String[]{"gino", "pippo", "pluto", "paperino", "topolino", "gina", "paperone"
-				, "ciccio", "roxy", "mirko"};
-		
-		String[] surnames = new String[]{"postino", "spazzino", "poliziotto", "insegnante", "operaio", "zen"
-				, "rossi", "verdi", "kjhso", "sdlgknf"};
+		setContentView(R.layout.activity_friendlist);		
 		
 		List<HashMap<String,String>> aList = new ArrayList<HashMap<String,String>>();
 		 
-		for(int i=0; i<names.length; i++) {
+		for(int i=0; i < names.length; i++) {
 			HashMap<String, String> hm = new HashMap<String,String>();
 			hm.put("name", names[i]);
 			hm.put("surname", surnames[i]);
+			hm.put("flag", Integer.toString(flags[i]));
 			aList.add(hm);
 		}
 		
-		String[] from = new String[]{"name", "surname"};
-		int[] to = new int[]{R.id.name_list_textview, R.id.surname_list_textview};
+		String[] from = new String[]{"name", "surname", "flag"};
+		int[] to = new int[]{R.id.name, R.id.surname, R.id.flag};
 		
 		
-		SimpleAdapter ad = new SimpleAdapter(getBaseContext(), aList, R.layout.listview_item_row, from, to);
-		ListView lv = (ListView) findViewById(R.id.friend_list_view);
+		SimpleAdapter ad = new SimpleAdapter(getBaseContext(), aList, R.layout.friend_listview_item, from, to);
+		ListView lv = (ListView) findViewById(R.id.friendlistview);
 		lv.setAdapter(ad);
 	}
 
