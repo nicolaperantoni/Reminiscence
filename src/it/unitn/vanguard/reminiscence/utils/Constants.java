007e6ecfdc76a36999955e7f23830d89e89358d5
Mirko <mirko.zaff@gmail.com>
2013-05-06 16:30:44 +0200

    gestione shared preferences

--- src/it/unitn/vanguard/reminiscence/utils/Constants.java
+++ src/it/unitn/vanguard/reminiscence/utils/Constants.java
@@ -11,8 +11,14 @@ public class Constants {
 	public static final int QUESTION_POPUP_SHOWING_TIME = 10000;
 	public static final int QUESTION_INTERVAL = 120000;
 	
-	public static final String LOUGO_DI_NASCITA_PREFERENCES_KEY="luogodinascita";
-	
+	public static final String LOUGO_DI_NASCITA_PREFERENCES_KEY = "luogodinascita";	
+	public static final String NAME_KEY = "name";
+	public final static String SURNAME_KEY = "surname";
+	public final static String MAIL_KEY = "mail";
+	public final static String DAY_KEY = "day";
+	public final static String MONTH_KEY = "month";
+	public final static String YEAR_KEY = "year";
+	public final static String PASSWORD_KEY = "password";
 	
 	public static enum imageType { PROFILE, STORY };
 }
