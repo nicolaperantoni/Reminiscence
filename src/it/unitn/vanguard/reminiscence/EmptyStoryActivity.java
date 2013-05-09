29e976101d7add07f8feab7e32241c80fa59072a
jibbo <posta.giovanni.defrancesco@gmail.com>
2013-05-09 22:56:50 +0200

    boh! dio cane eclipse

--- src/it/unitn/vanguard/reminiscence/EmptyStoryActivity.java
+++ src/it/unitn/vanguard/reminiscence/EmptyStoryActivity.java
@@ -16,6 +16,8 @@ import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import eu.giovannidefrancesco.DroidTimeline.widget.HorizontalListView;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
@@ -28,8 +30,11 @@ import android.text.TextWatcher;
 import android.util.Base64;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -44,30 +49,32 @@ public class EmptyStoryActivity extends BaseActivity implements OnTaskFinished {
 	private EditText mDescriptionEt;
 	private EditText mYearEt;
 	private Button mAddBtn;
-	private Button mMediaButton;
+	private ImageView mMediaButton;
+	private HorizontalListView mMedias;
+	private ArrayList<ImageView> imgs;
+	private ImageViewAdapter mAdapter;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		
-		context = EmptyStoryActivity.this;
+		context = this;
 		
 		setContentView(R.layout.fragment_emptystory);
+		
 		mNoStoryTv = (TextView) findViewById(R.id.emptystory_nostory_tv);
 		mTitleEt = (EditText) findViewById(R.id.emptystory_title_et);
 		mDescriptionEt = (EditText) findViewById(R.id.emptystory_desc_et);
 		mAddBtn = (Button) findViewById(R.id.emptystory_add_btn);
-		mMediaButton = (Button) findViewById(R.id.emptystory_btnPhoto);
+		mMediaButton = (ImageView) findViewById(R.id.emptystory_addMedia);
 		mYearEt = (EditText) findViewById(R.id.emptystory_year_et);
 		mYearEt.setText(getIntent().getExtras().getString(YEAR_PASSED_KEY));
-
+		
+		mMedias = (HorizontalListView) findViewById(R.id.emptystories_imgs_hlv);
+		imgs = new ArrayList<ImageView>();
+		mAdapter = new ImageViewAdapter();
+		//mMedias.setAdapter(mAdapter);
 		initializeListeners();
-	}
-
-	@Override
-	public void onStart() {
-		super.onStart();
-
 	}
 
 	private void initializeListeners() {
@@ -100,7 +107,7 @@ public class EmptyStoryActivity extends BaseActivity implements OnTaskFinished {
 			public void onClick(View v) {
 				String year = mYearEt.getText().toString();
 				if (FinalFunctionsUtilities
-						.isDeviceConnected(EmptyStoryActivity.this))
+						.isDeviceConnected(context))
 					if (checkYearInput(year))
 						new AddStoryTask(EmptyStoryActivity.this).execute(year,
 								mDescriptionEt.getText().toString(), mTitleEt
@@ -166,8 +173,12 @@ public class EmptyStoryActivity extends BaseActivity implements OnTaskFinished {
 				try {
 					// TODO upload and view of the photo
 					// new UploadPhotoTask(this, Constants.imageType.STORY,
-					// EmptyStoryActivity.this).execute(encodedImage, story_id);
+					// context).execute(encodedImage, story_id);
 					// view.setImageBitmap(mBitmap);
+					ImageView image = new ImageView(context);
+					image.setImageBitmap(mBitmap);
+					imgs.add(image);
+					mAdapter.notifyDataSetChanged();
 				} catch (Exception e) {
 
 					Log.e("log_tag", "Error in http connection " + e.toString());
@@ -187,7 +198,7 @@ public class EmptyStoryActivity extends BaseActivity implements OnTaskFinished {
 	private boolean checkYearInput(String year) {
 		int bornyear = Integer.parseInt(FinalFunctionsUtilities
 				.getSharedPreferences(Constants.YEAR_KEY,
-						EmptyStoryActivity.this));
+						context));
 		int actualyear = Calendar.getInstance().get(Calendar.YEAR);
 
 		try {
@@ -210,5 +221,29 @@ public class EmptyStoryActivity extends BaseActivity implements OnTaskFinished {
 	private void showToast(int resource) {
 		Toast.makeText(this.getApplicationContext(),
 				getResources().getString(resource), Toast.LENGTH_SHORT).show();
+	}
+	
+	private class ImageViewAdapter extends BaseAdapter{
+
+		@Override
+		public int getCount() {
+			return imgs.size();
+		}
+
+		@Override
+		public Object getItem(int arg0) {
+			return arg0;
+		}
+
+		@Override
+		public long getItemId(int arg0) {
+			return arg0;
+		}
+
+		@Override
+		public View getView(int arg0, View arg1, ViewGroup arg2) {
+			return imgs.get(arg0);
+		}
+		
 	}
 }
